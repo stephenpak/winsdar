bd1969468091e8c76a7e418a267bd803fab4461c
Stephen Pak <stephenjpak@gmail.com>
2022-06-10 16:24:32 -0700

    6-10-22-4:23pm

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
-const app = require('express');
+const express = require('express');
 const app = express();
 const db = require('./models');
 const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`listening on: http//localhost:${PORT}`);
+  });
+});
