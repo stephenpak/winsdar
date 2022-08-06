0ddbd79e97c15bccf1fbee6d1668774a9ff86451
zanatooo <m8elzanaty@gmail.com>
2022-08-06 09:44:12 -1000

    added more products/images

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -7,7 +7,7 @@ const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">{heroBanner.smallText}</p>
+        <p className="Beats anything in the market">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
         <img src={urlFor(heroBanner.image)} alt="Demon Slayer" className="hero-banner-image" />
