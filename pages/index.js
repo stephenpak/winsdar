1d62419bd00abef7d4933256ecbd4ff2858eccbf
Stephen Pak <stephenjpak@gmail.com>
2022-08-09 03:49:21 -0700

    8-9-22

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const Home = ({ products, bannerData }) => {
         ))}
       </div>
 
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
   );
 };
