0ee8b19aca7e65932b017a20c03811cac7b8eb09
zanatooo <m8elzanaty@gmail.com>
2022-08-09 18:10:49 -0700

    fixing the token and project ID

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cdcefcd2ff8ac4bf"; }
+/******/ 		__webpack_require__.h = function() { return "29507a72d3167683"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
-          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/013h5p0c/production/').replace('-webp', '.webp');
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/s6qdatjx/production/').replace('-webp', '.webp');
 
           return {
             price_data: { 
